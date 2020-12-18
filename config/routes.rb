abe71e55358d50582cb87858b230eceb90e1f959
github username <github email address>
2020-12-18 14:51:23 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :products, only: :index
+  get '/', to: "application#cart"
+  get '/products', to: "products#index"
+  post '/', to: "products#add_to_cart"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
